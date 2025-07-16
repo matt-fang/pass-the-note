8e05066f92e13c53bb354338e4b1829d610f16d2
matt-fang <matthewfang.calif@gmail.com>
2025-07-17 01:07:17 +0200

    add signature bottom padding

--- src/components/SignedNote.tsx
+++ src/components/SignedNote.tsx
@@ -77,7 +77,7 @@ export default function SignedNote({
                 alignItems: "center",
                 justifyContent: "center",
                 transformOrigin: "center",
-                marginBottom: "10px",
+                marginBottom: "20px",
               }}
             >
               {shouldShowSignature ? (
